9ee87d3f7dc7671242d74e0015213b3f84a3d6b2
Alex Kobilianskiy <alex_kobilianskiy@bigmir.net>
2016-12-03 03:38:26 +0200

    testing templates created

--- src/Blog/BlogBundle/Controller/PageController.php
+++ src/Blog/BlogBundle/Controller/PageController.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alex
- * Date: 03.12.16
- * Time: 3:10
- */
+
+namespace Blog\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PageController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('BlogBundle:Page:index.html.twig');
+    }
+}
