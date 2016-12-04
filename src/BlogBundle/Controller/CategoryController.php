172a6b4c56ec7b8a581d23af60904beffc83ac9d
xFLy3r <datus3r@gmail.com>
2016-12-04 20:41:14 +0200

    Added Category Controller and entities

---
+++ src/BlogBundle/Controller/CategoryController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BlogBundle\Controller;
+
+use BlogBundle\Entity\PostEntity;
+use BlogBundle\Entity\CategoryEntity;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CategoryController extends Controller
+{
+
+    /**
+     * Matches /category/* and get posts from category #{id}
+     *
+     * @Route("/category/{id}", name="category_show", requirements={"id": "\d+"})
+     * @Method("GET")
+     */
+    public function showAction(Request $request)
+    {
+        $entity = new PostEntity();
+        $posts = $entity->getPosts();
+        return $this->render('@Blog/Post/index.html.twig', array(
+            'title' => 'Category #' . $request->get('id'),
+            'posts' => $posts,
+            'categories' => CategoryEntity::getCategories(),
+            'page' => 'category',
+        ));
+    }
+
+
+}
---
+++ src/BlogBundle/Entity/PostEntity.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace BlogBundle\Entity;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PostEntity {
+
+
+    public function __construct()
+    {
+    }
+
+    public function getPosts()
+    {
+        $faker = \Faker\Factory::create();
+        for ($i = 1; $i < 6; $i++) {
+            $post[$i]['id'] = $i;
+            $post[$i]['title'] = $faker->text(6);
+            $post[$i]['text'] = $faker->text(200);
+            $post[$i]['datetime'] = $faker->date() . ' ' . $faker->time();
+        }
+        return $post;
+    }
+    
+    public function getPost($id)
+    {
+        if ($id > 10 || $id < 1) {
+            return false;
+        }
+            $faker = \Faker\Factory::create();
+            $post['id'] = $id;
+            $post['title'] = $faker->text(6);
+            $post['text'] = $faker->text(200);
+            $post['datetime'] = $faker->date() . ' ' . $faker->time();
+            return $post;
+    }
+}
