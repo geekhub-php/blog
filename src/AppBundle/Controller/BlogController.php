6ece80fccbed1d937fe32ac63081291820e468b1
Ocheretnyi <sasha.ocheretniy@mail.ru>
2016-12-05 21:44:01 +0200

    add template

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -5,17 +5,18 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Faker\Factory;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Template("default/index.html.twig")
      */
     public function indexAction()
     {
 
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        $faker = Faker\Factory::create();
+        echo "Hi {$faker->name()}!";
     }
 }
