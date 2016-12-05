78576bd5e7ac9d948a04ad523a516c47b1f329ab
Ocheretnyi <sasha.ocheretniy@mail.ru>
2016-12-05 21:44:29 +0200

    add template

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -1,22 +1,47 @@
 <?php
 
-namespace AppBundle\Controller;
 
+namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Faker\Factory;
+use Faker;
 
-class DefaultController extends Controller
+
+class BlogController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
-     * @Template("default/index.html.twig")
+     * @Route("/article", name="article")
+     * @Template()
      */
-    public function indexAction()
+    public function articleAction()
     {
+        $faker = Faker\Factory::create('ru_RU');
+        $Article = [];
+        for ($i=0;$i<8;$i++)
+        {
+            $arr = [
+                'img' => $faker->imageUrl($width = 640, $height = 480),
+                'date' => $faker->date($format = 'Y-m-d', $max = 'now'),
+                'time' => $faker -> time($format = 'H:i:s', $max = 'now'),
+                'title' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+                'article' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true)
 
-        $faker = Faker\Factory::create();
-        echo "Hi {$faker->name()}!";
+            ];
+            array_push($Article, $arr);
+        }
+        return [
+            'Article' => $Article
+        ];
+    }
+
+    /**
+     * @Route("/about", name="about_me")
+     * @Template()
+     */
+    public function aboutAction()
+    {
+        return $this->render('AppBundle:Blog:about.html.twig');
     }
 }
