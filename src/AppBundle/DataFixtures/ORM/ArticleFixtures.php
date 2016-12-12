999fe63a43ae67e1d918f835129a6a53c1b36e25
Alex Kobilianskiy <alex_kobilianskiy@bigmir.net>
2016-12-12 11:05:33 +0200

    indexAction modified (in PageController)

--- src/AppBundle/DataFixtures/ORM/ArticleFixtures.php
+++ src/AppBundle/DataFixtures/ORM/ArticleFixtures.php
@@ -4,22 +4,23 @@ namespace AppBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use AppBundle\Entity\Article;
+use Faker;
 
 class ArticleFixtures implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $article1 = new article();
-        $article1->setTitle('Title1');
-        $article1->setContent('Etiam vehicula nunc non leo hendrerit commodo. Vestibulum 
-vulputate mauris eget erat congue dapibus imperdiet justo scelerisque. Nulla consectetur
-tempus nisl vitae viverra. Cras el mauris eget erat congue dapibus imperdiet justo 
-scelerisque. Nulla consectetur tempus nisl vitae viverra. Cras elementum molestie vestibulum.
-Morbi id quam nisl. ');
-        $article1->setImage('image1.jpg');
-        $article1->setDate(new \DateTime());
-        $manager->persist($article1);
-        $manager->flush();
+        $faker = Faker\Factory::create();
 
+        for ($i = 1; $i <= 20; $i++) {
+            $article = new article();
+            $article->setTitle($faker->catchPhrase);
+            $article->setContent($faker->text($maxNbChars = 1000));
+            $article->setImage('image1.jpg');
+            $article->setDate($faker->dateTimeThisYear($max = 'now'));
+            $manager->persist($article);
+            $manager->flush();
+
+        }
     }
 }
