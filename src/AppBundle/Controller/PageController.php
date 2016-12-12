6f5d78da20b96b5294635a77e05ceee316dba8b5
Alex Kobilianskiy <alex_kobilianskiy@bigmir.net>
2016-12-12 05:50:13 +0200

    added fixtures, templates and some functionality

--- src/AppBundle/Controller/PageController.php
+++ src/AppBundle/Controller/PageController.php
@@ -1,7 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alex
- * Date: 11.12.16
- * Time: 12:18
- */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PageController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     * @Method({"GET"})
+     */
+
+    public function indexAction()
+    {
+       return $this->render('Page/index.html.twig');
+
+    }
+
+    /**
+     * @Route("/about", name="about")
+     * @Method({"GET"})
+     */
+    public function aboutAction()
+    {
+        return $this->render('Page/about.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     * @Method({"GET"})
+     */
+    public function contactAction()
+    {
+        return $this->render('Page/contact.html.twig');
+    }
+
+}
