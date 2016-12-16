d1824771457d99517aad1f19b702d41888d9431b
Vladimir Nimchyn <nima.vova@gmail.com>
2016-12-16 20:59:08 +0200

    replacing one  to bidirectional relation

---
+++ src/AppBundle/Entity/Comment.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Comment
+ *
+ * @ORM\Table(name="comment")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CommentRepository")
+ */
+class Comment
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="text")
+     */
+    private $text;
+
+    /**
+     * @var string
+     *
+     * @ORM\ManyToOne(targetEntity="Post")
+     */
+    private $post;
+
+    /**
+     * @var string
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     */
+    private $user;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="date", type="string", length=10)
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="enabled", type="string", length=10)
+     */
+    private $enabled;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return Comment
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set post
+     *
+     * @param string $post
+     *
+     * @return Comment
+     */
+    public function setPost($post)
+    {
+        $this->post = $post;
+
+        return $this;
+    }
+
+    /**
+     * Get post
+     *
+     * @return string
+     */
+    public function getPost()
+    {
+        return $this->post;
+    }
+
+    /**
+     * Set user
+     *
+     * @param string $user
+     *
+     * @return Comment
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return string
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set date
+     *
+     * @param string $date
+     *
+     * @return Comment
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return string
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set enabled
+     *
+     * @param string $enabled
+     *
+     * @return Comment
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    /**
+     * Get enabled
+     *
+     * @return string
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+}
---
+++ src/AppBundle/Entity/Post.php
@@ -0,0 +1,369 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Post
+ *
+ * @ORM\Table(name="post")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PostRepository")
+ */
+class Post
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="text")
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="body", type="text")
+     */
+    private $body;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="hashtag", type="string", length=100)
+     */
+    private $hashtag;
+
+    /**
+     * @var string
+     *
+     * @ORM\ManyToMany(targetEntity="User", inversedBy="posts")
+     */
+    private $authors;
+
+    /**
+     *
+     * @ORM\ManyToOne(targetEntity="Category")
+     */
+    private $category;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="rating", type="integer")
+     */
+    private $rating;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="dataCreate", type="string", length=10)
+     */
+    private $dataCreate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="enabled", type="string", length=10)
+     */
+    private $enabled;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="dataEdit", type="string", length=10)
+     */
+    private $dataEdit;
+
+    public function __construct() {
+        $this->authors = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Post
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Post
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set body
+     *
+     * @param string $body
+     *
+     * @return Post
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * Get body
+     *
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Set hashtag
+     *
+     * @param string $hashtag
+     *
+     * @return Post
+     */
+    public function setHashtag($hashtag)
+    {
+        $this->hashtag = $hashtag;
+
+        return $this;
+    }
+
+    /**
+     * Get hashtag
+     *
+     * @return string
+     */
+    public function getHashtag()
+    {
+        return $this->hashtag;
+    }
+
+    /**
+     * Set authors
+     *
+     * @param string $authors
+     *
+     * @return Post
+     */
+    public function setAuthors($authors)
+    {
+        $this->authors = $authors;
+
+        return $this;
+    }
+
+    /**
+     * Get authors
+     *
+     * @return string
+     */
+    public function getAuthors()
+    {
+        return $this->authors;
+    }
+
+    /**
+     * Set category
+     *
+     * @param string $category
+     *
+     * @return Post
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return string
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set rating
+     *
+     * @param integer $rating
+     *
+     * @return Post
+     */
+    public function setRating($rating)
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    /**
+     * Get rating
+     *
+     * @return int
+     */
+    public function getRating()
+    {
+        return $this->rating;
+    }
+
+    /**
+     * Set dataCreate
+     *
+     * @param string $dataCreate
+     *
+     * @return Post
+     */
+    public function setDataCreate($dataCreate)
+    {
+        $this->dataCreate = $dataCreate;
+
+        return $this;
+    }
+
+    /**
+     * Get dataCreate
+     *
+     * @return string
+     */
+    public function getDataCreate()
+    {
+        return $this->dataCreate;
+    }
+
+    /**
+     * Set enabled
+     *
+     * @param string $enabled
+     *
+     * @return Post
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    /**
+     * Get enabled
+     *
+     * @return string
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * Set dataEdit
+     *
+     * @param string $dataEdit
+     *
+     * @return Post
+     */
+    public function setDataEdit($dataEdit)
+    {
+        $this->dataEdit = $dataEdit;
+
+        return $this;
+    }
+
+    /**
+     * Get dataEdit
+     *
+     * @return string
+     */
+    public function getDataEdit()
+    {
+        return $this->dataEdit;
+    }
+
+    /**
+     * Add author
+     *
+     * @param \AppBundle\Entity\User $author
+     *
+     * @return Post
+     */
+    public function addAuthor(\AppBundle\Entity\User $author)
+    {
+        $this->authors[] = $author;
+
+        return $this;
+    }
+
+    /**
+     * Remove author
+     *
+     * @param \AppBundle\Entity\User $author
+     */
+    public function removeAuthor(\AppBundle\Entity\User $author)
+    {
+        $this->authors->removeElement($author);
+    }
+}
